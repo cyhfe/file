31890700bbf0e54f47085ecf14a7515f40a4fbdf
chenyuhao <cccchenyuhao@gmail.com>
2024-01-30 09:21:19 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <form action={uploadFile}>
       <input type="file" name="file" id="file" />
-      <button>submit</button>
+      <button type="submit">submit</button>
     </form>
   );
 }
